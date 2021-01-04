e52b963276a51a2530357d928a33b8e58d44f240
余艳辉 <913918146@qq.com>
2021-01-04 16:18:55 +0800

    Initial commit

---
+++ app/src/main/java/com/ke/wentong_common/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ke.wentong_common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
